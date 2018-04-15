4bd0f91f4515ceee236632dec880c55661a870ab
statusfailed <sf@statusfailed.com>
2018-04-15 17:08:14 +0100

    make Id part of Expr

--- src/SMC/Expr.hs
+++ src/SMC/Expr.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TupleSections #-}
 module SMC.Expr where
 
 import Control.Monad
@@ -9,14 +10,26 @@ import Control.Monad.Catch
 import SMC.Hypergraph
 import SMC.Match
 
+import Data.Function (on)
+import Data.List (sort, groupBy)
+
+import Data.Map (Map(..), (!))
+import qualified Data.Map as Map
+
+-- we need stronglyConnComp to renumber nodes when identity or twist terms
+-- are encountered.
+import Data.Graph (stronglyConnComp, flattenSCC)
+
 data Expr e
-  = Generator e
+  = Id
+  | Generator e
   | Seq (Expr e) (Expr e)
   | Par (Expr e) (Expr e)
   deriving(Eq, Ord, Read, Show)
 
 -- | Calculate "type" of expression given a function returning types of generators.
 typeOf :: (MonadPlus m) => (e -> m (Int, Int)) -> Expr e -> m (Int, Int)
+typeOf f Id = return (1, 1)
 typeOf f (Generator e) = f e
 typeOf f (Seq x y) = do
   (i,j) <- typeOf f x
@@ -28,16 +41,28 @@ typeOf f (Par x y) = do
   (m,n) <- typeOf f y
   return (i+m, j+n)
 
+data BuildState e = BuildState
+  { numVars    :: Int
+  , newEdges   :: [Hyperedge e] -- ^ NOTE: this should really be in a WriterT
+  , equalities :: [(Int, Int)] -- ^ Which "nodes" are actually the same
+  } deriving(Eq, Ord, Read, Show)
+
+
+-- | Add a new node to the BuildState.
 freshNode :: MonadState (BuildState e) m => m Int
 freshNode = do
-  (BuildState i s) <- get
-  put $ BuildState (i+1) s
+  (BuildState i s eqs) <- get
+  put $ BuildState (i+1) s eqs
   return i
 
-data BuildState e = BuildState
-  { numVars  :: Int
-  , newEdges :: [Hyperedge e] -- ^ NOTE: this should really be in a WriterT
-  } deriving(Eq, Ord, Read, Show)
+-- | Assert that two nodes are equal
+addEquality :: Int -> Int -> BuildState e -> BuildState e
+addEquality i j (BuildState v e eqs) = BuildState v e $ (i, j) : eqs
+
+addEdge :: Hyperedge e -> BuildState e -> BuildState e
+addEdge e (BuildState n es eqs) = BuildState n (e:es) eqs
+
+--------- Build a graph from a term ------------
 
 -- | Build a 'Hypergraph v e' from an 'Expr e'
 -- NOTE: This is really dumb - it recomputes types loads of times!
@@ -48,26 +73,42 @@ build
   -> ([Int], [Int])      -- ^ Context nodes - where this term will connect to.
   -> Expr e              -- ^ Expression to graphify
   -> m ()                -- ^ Result contained in state.
-build f (ls, rs) t@(Generator e) = do
-  (BuildState n es) <- get
-  put $ BuildState n (mkEdge e ls rs : es)
 
+-- | Identity means that nodes (ls, rs) are actually the same node.
+-- We make a note of this, and renumber nodes later.
+build f (ls, rs) Id = do
+  case (ls, rs) of
+    (l:[], r:[]) -> modify $ addEquality (head ls) (head rs)
+    _ -> error $ "The unpossible happened: " ++ show (ls, rs)
+
+-- | Build a generator: must have same type as passed-in context (ls, rs)
+build f (ls, rs) t@(Generator e) = do
+  modify (addEdge $ mkEdge e ls rs)
+
+-- | Sequential composition of terms
 build f (ls, rs) t@(Seq x y) = do
   (i, j) <- typeOf f x
   (m, n) <- typeOf f y
+
+  -- Ensure RHS of left term == LHS of right term
   guard (j == m)
 
+  -- Make j == m nodes to connect the two terms
   ns <- replicateM m freshNode
 
+  -- Pass the new nodes to the "built" graph of x and y
   build f (ls, ns) x
   build f (ns, rs) y
 
+-- | Parallel composition of terms: 
 build f (ls, rs) t@(Par x y) = do
   (i, j) <- typeOf f x
   (m, n) <- typeOf f y
 
   build f (take i ls, take j rs) x
   build f (drop i ls, drop j rs) y
+
+
 
 range :: Int -> Int -> [Int]
 range from to
@@ -82,18 +123,61 @@ toGraph'
   -> m ()
 toGraph' f e = do
   (i, j) <- typeOf f e
-  put $ BuildState (i+j) []
-  let ctx = (range 0 (i-1), range i (i+j))
+  put $ BuildState (i+j) [] []
+  let ctx = (range 0 i, range i (i+j))
   build f ctx e
 
-buildStateToGraph (BuildState n es) = mkGraph vs (reverse es)
-  where vs = [0..n-1]
 
-toGraph :: (e -> Maybe (Int, Int)) -> Expr e -> Maybe (Hypergraph Int e)
+toGraph
+  :: Show e
+  => (e -> Maybe (Int, Int)) -> Expr e -> Maybe (Hypergraph Int e)
 toGraph f e =
   case execStateT (toGraph' (g . f) e) s of
     Just s  -> Just (buildStateToGraph s)
     Nothing -> Nothing
   where
-    s = BuildState 0 [] -- note this is immediately overwritten by toGraph'
+    s = BuildState 0 [] [] -- note this is immediately overwritten by toGraph'
     g = maybe mzero return
+
+-- | Turn a BuildState into a Graph
+-- TODO: renumber here!
+buildStateToGraph :: Show e => BuildState e -> Hypergraph Int e
+buildStateToGraph bs = mkGraph vs (reverse es)
+  where
+    (BuildState n es eqs) = renumber bs
+    vs = [0..n-1]
+
+-- | Map a BuildState with a set of node equalities into one with no equalities
+-- by renumbering all the nodes.
+-- 0. For n nodes
+-- 1. Generate set of m < n equivalence classes (the strongly connected
+--    components)
+-- 2. Number each class [0..m]
+-- 3. Generate a map (NodeId -> EquivalenceClassNumber)
+-- 4. Replace each node in the graph with its equivalence class
+renumber :: Show e => BuildState e -> BuildState e
+renumber s@(BuildState n es eqs) =
+  BuildState (length sccs) (fmap (renumberEdge m) es) []
+  where
+    -- Data.Graph assumes directional edges, so we put fwd + back edges.
+    -- we also ensure every node is equal to itself.
+    g (i,j) = [(i,j), (j,i)]
+    eqs' = (eqs >>= g) ++ (let ixs = [0..n-1] in zip ixs ixs)
+
+    f pairs = let i = fst (head pairs) in (i, i, fmap snd pairs)
+    xs = fmap f . groupBy ( (==) `on` fst ) . sort $ eqs'
+
+    sccs = zipWith (,) [0..] . fmap flattenSCC . stronglyConnComp $ xs
+
+    -- mapping from NodeId -> ConnectedComponent
+    m :: Map Int Int
+    m = let kvp (i, js) = fmap (,i) js
+        in  Map.fromList . concat $ fmap kvp sccs
+
+-- | Renumber a hyperedge
+renumberEdge :: Map Int Int -> Hyperedge e -> Hyperedge e
+renumberEdge m (Hyperedge v d c) = Hyperedge v (fmap look d) (fmap look c)
+  where
+    look i = case Map.lookup i m of
+      Nothing -> error $ "couldn't find " ++ show i ++ " in map " ++ show m
+      Just  r -> r
--- test/TestExpr.hs
+++ test/TestExpr.hs
@@ -23,10 +23,14 @@ testExpr = testGroup "test Expr" unitTests
 
 unitTests =
   [ let
-      e = Seq (Par (Generator Id) (Generator Add)) (Generator Add)
+      e = Seq (Par Id (Generator Add)) (Generator Add)
       (Just g) = toGraph (return . iaType) $ double e
       (Just p) = toGraph (return . iaType) e
     in testMatches "test doubling has at least two matches" ((>=2) . length) g p
+  , let
+      msg = "toGraph Id has one node and no edges"
+      (Just g) = toGraph return Id
+    in testCase msg . assertBool msg $ length (nodes g) == 1 && length (edges g) == 0
   ]
 
 double :: Expr e -> Expr e
@@ -35,10 +39,8 @@ double t = Par t t
 example = toGraph return $ Seq (Generator (1,2)) (Generator (2,1))
 example2 = toGraph return $ Par (Generator (1,1)) (Generator (1,1))
 
--- A simple system with two generators, Id and Add
-data IA = Id | Add
+-- A simple system with one generator, Add
+data IA = Add
   deriving(Eq, Ord, Read, Show)
 
-iaType Id  = (1,1)
 iaType Add = (2,1)
-
