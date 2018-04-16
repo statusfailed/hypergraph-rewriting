c0f2db789b09585e84519179e3934c1430c528cf
statusfailed <sf@statusfailed.com>
2018-04-16 21:44:29 +0100

    fix convexity test; add comments

--- test/TestPaper.hs
+++ test/TestPaper.hs
@@ -25,12 +25,21 @@ import Utils (testMatches)
 testPaper = testGroup "examples from paper" unitTests
 
 unitTests =
+  -- Test that matching the LHS of example 4.8 in its context yields exactly one match;
+  -- this tests matching *without* the convexity and boundary complement restrictions
   [ testMatches "example 4.8" ((==1) . length) eaGraph eaLHS
+
+  -- Same as above, but for example 5.3. Ensure exactly one match for LHS,
+  -- assuming no convexity condition.
   , testMatches "example 5.3" ((==1) . length) ebGraph ebLHS
-  , let f g (Matching ns es) = not . convex g . fmap snd . Bimap.toList $ ns
+
+  -- Test for example 5.3, but with ensure convexity condition is true
+  , let f g (Matching ns es) = convex g . fmap snd . Bimap.toList $ ns
         h = filter (f ebGraph)
     in  testMatches "example 5.3 (convex)" ((==0) . length . h) ebGraph ebLHS
   ]
+
+------------ Example 4.8 -------------
 
 -- | Signature for Example 4.8
 -- the "A" in EA is just to distinguish from Example 5.3 later. Kinda hacky I
@@ -54,7 +63,8 @@ Just eaGraph = toGraph (return . eaType) expr
 eaLHS :: Hypergraph Int EA
 (Just eaLHS) = toGraph (return . eaType) Id
 
-results = match eaGraph eaLHS
+
+------------ Example 5.3 -------------
 
 data EB = EB1 | EB2 | EB3 | EB4
   deriving(Eq, Ord, Read, Show)
@@ -66,6 +76,7 @@ ebType x = case x of
   EB3 -> (1, 1)
   EB4 -> (1, 1)
 
+-- Left-hand-side of the rewrite rule from example 5.3
 ebLHS :: Hypergraph Int EB
 Just ebLHS = toGraph (return . ebType) expr
   where
@@ -75,6 +86,7 @@ Just ebLHS = toGraph (return . ebType) expr
       , Par Id (Generator EB2)
       ]
 
+-- Graph (context) from example 5.3
 ebGraph :: Hypergraph Int EB
 Just ebGraph = toGraph (return . ebType) expr
   where
